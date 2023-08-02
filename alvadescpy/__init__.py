8ee7ab89bd10af596796b2cf05287d09cbeb4577
tjkessler <travis.j.kessler@gmail.com>
2023-08-01 23:42:12 -0400

    Version now pulls from 'pkg_resources'

--- alvadescpy/__init__.py
+++ alvadescpy/__init__.py
@@ -1,4 +1,5 @@
 from alvadescpy.wrapper import alvadesc
 from alvadescpy.functions import smiles_to_descriptors
 from alvadescpy.wrapper import CONFIG
-__version__ = '0.1.2'
+import pkg_resources
+__version__ = pkg_resources.get_distribution('alvadescpy')
